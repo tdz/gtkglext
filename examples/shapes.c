ba4268a3f8d08f41c1ea554d35062faedb6fbb7a
Naofumi Yasufuku <naofumi@users.sourceforge.net>
2002-12-13 15:41:13 +0000

    Added materials menu.

--- examples/shapes.c
+++ examples/shapes.c
@@ -45,6 +45,100 @@ static const GLuint shape_teapot       = 8;
 static GLuint shape_list_base = 0;
 static GLuint shape_current = 8;
 
+typedef struct _MaterialProp
+{
+  GLfloat ambient[4];
+  GLfloat diffuse[4];
+  GLfloat specular[4];
+  GLfloat shininess;
+} MaterialProp;
+
+static MaterialProp mat_emerald = {
+  {0.0215, 0.1745, 0.0215, 1.0},
+  {0.07568, 0.61424, 0.07568, 1.0},
+  {0.633, 0.727811, 0.633, 1.0},
+  0.6
+};
+
+static MaterialProp mat_jade = {
+  {0.135, 0.2225, 0.1575, 1.0},
+  {0.54, 0.89, 0.63, 1.0},
+  {0.316228, 0.316228, 0.316228, 1.0},
+  0.1
+};
+
+static MaterialProp mat_obsidian = {
+  {0.05375, 0.05, 0.06625, 1.0},
+  {0.18275, 0.17, 0.22525, 1.0},
+  {0.332741, 0.328634, 0.346435, 1.0},
+  0.3
+};
+
+static MaterialProp mat_pearl = {
+  {0.25, 0.20725, 0.20725, 1.0},
+  {1.0, 0.829, 0.829, 1.0},
+  {0.296648, 0.296648, 0.296648, 1.0},
+  0.088
+};
+
+static MaterialProp mat_ruby = {
+  {0.1745, 0.01175, 0.01175, 1.0},
+  {0.61424, 0.04136, 0.04136, 1.0},
+  {0.727811, 0.626959, 0.626959, 1.0},
+  0.6
+};
+
+static MaterialProp mat_turquoise = {
+  {0.1, 0.18725, 0.1745, 1.0},
+  {0.396, 0.74151, 0.69102, 1.0},
+  {0.297254, 0.30829, 0.306678, 1.0},
+  0.1
+};
+
+static MaterialProp mat_brass = {
+  {0.329412, 0.223529, 0.027451, 1.0},
+  {0.780392, 0.568627, 0.113725, 1.0},
+  {0.992157, 0.941176, 0.807843, 1.0},
+  0.21794872
+};
+
+static MaterialProp mat_bronze = {
+  {0.2125, 0.1275, 0.054, 1.0},
+  {0.714, 0.4284, 0.18144, 1.0},
+  {0.393548, 0.271906, 0.166721, 1.0},
+  0.2
+};
+
+static MaterialProp mat_chrome = {
+  {0.25, 0.25, 0.25, 1.0},
+  {0.4, 0.4, 0.4, 1.0},
+  {0.774597, 0.774597, 0.774597, 1.0},
+  0.6
+};
+
+static MaterialProp mat_copper = {
+  {0.19125, 0.0735, 0.0225, 1.0},
+  {0.7038, 0.27048, 0.0828, 1.0},
+  {0.256777, 0.137622, 0.086014, 1.0},
+  0.1
+};
+
+static MaterialProp mat_gold = {
+  {0.24725, 0.1995, 0.0745, 1.0},
+  {0.75164, 0.60648, 0.22648, 1.0},
+  {0.628281, 0.555802, 0.366065, 1.0},
+  0.4
+};
+
+static MaterialProp mat_silver = {
+  {0.19225, 0.19225, 0.19225, 1.0},
+  {0.50754, 0.50754, 0.50754, 1.0},
+  {0.508273, 0.508273, 0.508273, 1.0},
+  0.4
+};
+
+static MaterialProp *mat_current = &mat_silver;
+
 static float view_quat_diff[4] = { 0.0, 0.0, 0.0, 1.0 };
 static float view_quat[4] = { 0.0, 0.0, 0.0, 1.0 };
 static float view_scale = 1.0;
@@ -197,44 +291,6 @@ configure_event (GtkWidget         *widget,
   return TRUE;
 }
 
-/* Render shape */
-void
-render_shape (GLfloat x, GLfloat y,
-              GLfloat ambr, GLfloat ambg, GLfloat ambb,
-              GLfloat difr, GLfloat difg, GLfloat difb,
-              GLfloat specr, GLfloat specg, GLfloat specb,
-              GLfloat shine,
-              GLuint list)
-{
-  float mat[4];
-
-  glPushMatrix();
-
-    glTranslatef(x, y, 0.0);
-
-    mat[0] = ambr;
-    mat[1] = ambg;
-    mat[2] = ambb;
-    mat[3] = 1.0;
-    glMaterialfv(GL_FRONT, GL_AMBIENT, mat);
-
-    mat[0] = difr;
-    mat[1] = difg;
-    mat[2] = difb;
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat);
-
-    mat[0] = specr;
-    mat[1] = specg;
-    mat[2] = specb;
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat);
-
-    glMaterialf(GL_FRONT, GL_SHININESS, shine * 128.0);
-
-    glCallList(list);
-
-  glPopMatrix();
-}
-
 static gboolean
 expose_event (GtkWidget      *widget,
 	      GdkEventExpose *event,
@@ -261,12 +317,11 @@ expose_event (GtkWidget      *widget,
   glMultMatrixf (&m[0][0]);
 
   /* Render shape */
-  render_shape (0.0, 0.0,
-                0.0, 0.0, 0.0,
-                0.55, 0.55, 0.55,
-                0.70, 0.70, 0.70,
-                0.25,
-                shape_list_base + shape_current);
+  glMaterialfv (GL_FRONT, GL_AMBIENT, mat_current->ambient);
+  glMaterialfv (GL_FRONT, GL_DIFFUSE, mat_current->diffuse);
+  glMaterialfv (GL_FRONT, GL_SPECULAR, mat_current->specular);
+  glMaterialf (GL_FRONT, GL_SHININESS, mat_current->shininess * 128.0);
+  glCallList (shape_list_base + shape_current);
 
   /* Swap buffers */
   if (gdk_gl_drawable_is_double_buffered (gldrawable))
@@ -474,8 +529,14 @@ change_shape (GtkMenuItem  *menuitem,
               const GLuint *shape)
 {
   shape_current = *shape;
-
   init_view ();
+}
+
+static void
+change_material (GtkMenuItem  *menuitem,
+                 MaterialProp *mat)
+{
+  mat_current = mat;
 }
 
 /* For popup menu. */
@@ -499,72 +560,186 @@ button_press_event_popup_menu (GtkWidget      *widget,
 static GtkWidget *
 create_popup_menu (GtkWidget *drawing_area)
 {
+  GtkWidget *shapes_menu;
+  GtkWidget *materials_menu;
   GtkWidget *menu;
   GtkWidget *menu_item;
 
-  menu = gtk_menu_new ();
+  /*
+   * Shapes submenu.
+   */
+
+  shapes_menu = gtk_menu_new ();
 
   /* Cube */
   menu_item = gtk_menu_item_new_with_label ("Cube");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_cube);
   gtk_widget_show (menu_item);
 
   /* Sphere */
   menu_item = gtk_menu_item_new_with_label ("Sphere");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_sphere);
   gtk_widget_show (menu_item);
 
   /* Cone */
   menu_item = gtk_menu_item_new_with_label ("Cone");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_cone);
   gtk_widget_show (menu_item);
 
   /* Torus */
   menu_item = gtk_menu_item_new_with_label ("Torus");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_torus);
   gtk_widget_show (menu_item);
 
   /* Tetrahedron */
   menu_item = gtk_menu_item_new_with_label ("Tetrahedron");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_tetrahedron);
   gtk_widget_show (menu_item);
 
   /* Octahedron */
   menu_item = gtk_menu_item_new_with_label ("Octahedron");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_octahedron);
   gtk_widget_show (menu_item);
 
   /* Dodecahedron */
   menu_item = gtk_menu_item_new_with_label ("Dodecahedron");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_dodecahedron);
   gtk_widget_show (menu_item);
 
   /* Icosahedron */
   menu_item = gtk_menu_item_new_with_label ("Icosahedron");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_icosahedron);
   gtk_widget_show (menu_item);
 
   /* Teapot */
   menu_item = gtk_menu_item_new_with_label ("Teapot");
-  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_menu_shell_append (GTK_MENU_SHELL (shapes_menu), menu_item);
   g_signal_connect (G_OBJECT (menu_item), "activate",
                     G_CALLBACK (change_shape), (gpointer) &shape_teapot);
+  gtk_widget_show (menu_item);
+
+  /*
+   * Materials submenu.
+   */
+
+  materials_menu = gtk_menu_new ();
+
+  /* Emerald */
+  menu_item = gtk_menu_item_new_with_label ("Emerald");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_emerald);
+  gtk_widget_show (menu_item);
+
+  /* Jade */
+  menu_item = gtk_menu_item_new_with_label ("Jade");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_jade);
+  gtk_widget_show (menu_item);
+
+  /* Obsidian */
+  menu_item = gtk_menu_item_new_with_label ("Obsidian");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_obsidian);
+  gtk_widget_show (menu_item);
+
+  /* Pearl */
+  menu_item = gtk_menu_item_new_with_label ("Pearl");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_pearl);
+  gtk_widget_show (menu_item);
+
+  /* Ruby */
+  menu_item = gtk_menu_item_new_with_label ("Ruby");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_ruby);
+  gtk_widget_show (menu_item);
+
+  /* Turquoise */
+  menu_item = gtk_menu_item_new_with_label ("Turquoise");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_turquoise);
+  gtk_widget_show (menu_item);
+
+  /* Brass */
+  menu_item = gtk_menu_item_new_with_label ("Brass");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_brass);
+  gtk_widget_show (menu_item);
+
+  /* Bronze */
+  menu_item = gtk_menu_item_new_with_label ("Bronze");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_bronze);
+  gtk_widget_show (menu_item);
+
+  /* Chrome */
+  menu_item = gtk_menu_item_new_with_label ("Chrome");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_chrome);
+  gtk_widget_show (menu_item);
+
+  /* Copper */
+  menu_item = gtk_menu_item_new_with_label ("Copper");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_copper);
+  gtk_widget_show (menu_item);
+
+  /* Gold */
+  menu_item = gtk_menu_item_new_with_label ("Gold");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_gold);
+  gtk_widget_show (menu_item);
+
+  /* Silver */
+  menu_item = gtk_menu_item_new_with_label ("Silver");
+  gtk_menu_shell_append (GTK_MENU_SHELL (materials_menu), menu_item);
+  g_signal_connect (G_OBJECT (menu_item), "activate",
+                    G_CALLBACK (change_material), &mat_silver);
+  gtk_widget_show (menu_item);
+
+  /* 
+   * Root popup menu.
+   */
+
+  menu = gtk_menu_new ();
+
+  /* Shapes */
+  menu_item = gtk_menu_item_new_with_label ("Shapes");
+  gtk_menu_item_set_submenu (GTK_MENU_ITEM (menu_item), shapes_menu);
+  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
+  gtk_widget_show (menu_item);
+
+  /* Materials */
+  menu_item = gtk_menu_item_new_with_label ("Materials");
+  gtk_menu_item_set_submenu (GTK_MENU_ITEM (menu_item), materials_menu);
+  gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_item);
   gtk_widget_show (menu_item);
 
   /* Quit */
