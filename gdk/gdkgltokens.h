67c8802c136b3953803e87f797903641ce6d933e
Naofumi Yasufuku <naofumi@users.sourceforge.net>
2003-01-08 09:13:13 +0000

    Tokens cleanup.

--- gdk/gdkgltokens.h
+++ gdk/gdkgltokens.h
@@ -82,52 +82,52 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_USE_GL                  = 1,  /* support GLX rendering */
-  GDK_GL_BUFFER_SIZE             = 2,  /* depth of the color buffer */
-  GDK_GL_LEVEL                   = 3,  /* level in plane stacking */
-  GDK_GL_RGBA                    = 4,  /* true if RGBA mode */
-  GDK_GL_DOUBLEBUFFER            = 5,  /* double buffering supported */
-  GDK_GL_STEREO                  = 6,  /* stereo buffering supported */
-  GDK_GL_AUX_BUFFERS             = 7,  /* number of aux buffers */
-  GDK_GL_RED_SIZE                = 8,  /* number of red component bits */
-  GDK_GL_GREEN_SIZE              = 9,  /* number of green component bits */
-  GDK_GL_BLUE_SIZE               = 10, /* number of blue component bits */
-  GDK_GL_ALPHA_SIZE              = 11, /* number of alpha component bits */
-  GDK_GL_DEPTH_SIZE              = 12, /* number of depth bits */
-  GDK_GL_STENCIL_SIZE            = 13, /* number of stencil bits */
-  GDK_GL_ACCUM_RED_SIZE          = 14, /* number of red accum bits */
-  GDK_GL_ACCUM_GREEN_SIZE        = 15, /* number of green accum bits */
-  GDK_GL_ACCUM_BLUE_SIZE         = 16, /* number of blue accum bits */
-  GDK_GL_ACCUM_ALPHA_SIZE        = 17, /* number of alpha accum bits */
+  GDK_GL_USE_GL                     = 1,  /* support GLX rendering */
+  GDK_GL_BUFFER_SIZE                = 2,  /* depth of the color buffer */
+  GDK_GL_LEVEL                      = 3,  /* level in plane stacking */
+  GDK_GL_RGBA                       = 4,  /* true if RGBA mode */
+  GDK_GL_DOUBLEBUFFER               = 5,  /* double buffering supported */
+  GDK_GL_STEREO                     = 6,  /* stereo buffering supported */
+  GDK_GL_AUX_BUFFERS                = 7,  /* number of aux buffers */
+  GDK_GL_RED_SIZE                   = 8,  /* number of red component bits */
+  GDK_GL_GREEN_SIZE                 = 9,  /* number of green component bits */
+  GDK_GL_BLUE_SIZE                  = 10, /* number of blue component bits */
+  GDK_GL_ALPHA_SIZE                 = 11, /* number of alpha component bits */
+  GDK_GL_DEPTH_SIZE                 = 12, /* number of depth bits */
+  GDK_GL_STENCIL_SIZE               = 13, /* number of stencil bits */
+  GDK_GL_ACCUM_RED_SIZE             = 14, /* number of red accum bits */
+  GDK_GL_ACCUM_GREEN_SIZE           = 15, /* number of green accum bits */
+  GDK_GL_ACCUM_BLUE_SIZE            = 16, /* number of blue accum bits */
+  GDK_GL_ACCUM_ALPHA_SIZE           = 17, /* number of alpha accum bits */
 
   /*
    * FBConfig-specific attributes.
    * [ GLX 1.3 and later ]
    */
-  GDK_GL_CONFIG_CAVEAT           = 0x20, /* Like visual_info VISUAL_CAVEAT_EXT */
-  GDK_GL_X_VISUAL_TYPE           = 0x22,
-  GDK_GL_TRANSPARENT_TYPE        = 0x23,
-  GDK_GL_TRANSPARENT_INDEX_VALUE = 0x24,
-  GDK_GL_TRANSPARENT_RED_VALUE   = 0x25,
-  GDK_GL_TRANSPARENT_GREEN_VALUE = 0x26,
-  GDK_GL_TRANSPARENT_BLUE_VALUE  = 0x27,
-  GDK_GL_TRANSPARENT_ALPHA_VALUE = 0x28,
-  GDK_GL_DRAWABLE_TYPE           = 0x8010,
-  GDK_GL_RENDER_TYPE             = 0x8011,
-  GDK_GL_X_RENDERABLE            = 0x8012,
-  GDK_GL_FBCONFIG_ID             = 0x8013,
-  GDK_GL_MAX_PBUFFER_WIDTH       = 0x8016,
-  GDK_GL_MAX_PBUFFER_HEIGHT      = 0x8017,
-  GDK_GL_MAX_PBUFFER_PIXELS      = 0x8018,
-  GDK_GL_VISUAL_ID               = 0x800B,
-  GDK_GL_SCREEN                  = 0x800C,
+  GDK_GL_CONFIG_CAVEAT              = 0x20, /* Like visual_info VISUAL_CAVEAT_EXT */
+  GDK_GL_X_VISUAL_TYPE              = 0x22,
+  GDK_GL_TRANSPARENT_TYPE           = 0x23,
+  GDK_GL_TRANSPARENT_INDEX_VALUE    = 0x24,
+  GDK_GL_TRANSPARENT_RED_VALUE      = 0x25,
+  GDK_GL_TRANSPARENT_GREEN_VALUE    = 0x26,
+  GDK_GL_TRANSPARENT_BLUE_VALUE     = 0x27,
+  GDK_GL_TRANSPARENT_ALPHA_VALUE    = 0x28,
+  GDK_GL_DRAWABLE_TYPE              = 0x8010,
+  GDK_GL_RENDER_TYPE                = 0x8011,
+  GDK_GL_X_RENDERABLE               = 0x8012,
+  GDK_GL_FBCONFIG_ID                = 0x8013,
+  GDK_GL_MAX_PBUFFER_WIDTH          = 0x8016,
+  GDK_GL_MAX_PBUFFER_HEIGHT         = 0x8017,
+  GDK_GL_MAX_PBUFFER_PIXELS         = 0x8018,
+  GDK_GL_VISUAL_ID                  = 0x800B,
+  GDK_GL_SCREEN                     = 0x800C,
 
   /*
    * Multisampling configuration attributes.
    * [ GLX 1.4 and later ]
    */
-  GDK_GL_SAMPLE_BUFFERS          = 100000,
-  GDK_GL_SAMPLES                 = 100001
+  GDK_GL_SAMPLE_BUFFERS             = 100000,
+  GDK_GL_SAMPLES                    = 100001
 
 } GdkGLConfigAttrib;
 
@@ -137,13 +137,13 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_BAD_SCREEN              = 1, /* screen # is bad */
-  GDK_GL_BAD_ATTRIBUTE           = 2, /* attribute to get is bad */
-  GDK_GL_NO_EXTENSION            = 3, /* no glx extension on server */
-  GDK_GL_BAD_VISUAL              = 4, /* visual # not known by GLX */
-  GDK_GL_BAD_CONTEXT             = 5, /* returned only by import_context EXT? */
-  GDK_GL_BAD_VALUE               = 6, /* returned only by glXSwapIntervalSGI? */
-  GDK_GL_BAD_ENUM                = 7  /* unused? */
+  GDK_GL_BAD_SCREEN                 = 1, /* screen # is bad */
+  GDK_GL_BAD_ATTRIBUTE              = 2, /* attribute to get is bad */
+  GDK_GL_NO_EXTENSION               = 3, /* no glx extension on server */
+  GDK_GL_BAD_VISUAL                 = 4, /* visual # not known by GLX */
+  GDK_GL_BAD_CONTEXT                = 5, /* returned only by import_context EXT? */
+  GDK_GL_BAD_VALUE                  = 6, /* returned only by glXSwapIntervalSGI? */
+  GDK_GL_BAD_ENUM                   = 7  /* unused? */
 } GdkGLConfigError;
 
 /*
@@ -152,7 +152,7 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_DONT_CARE               = 0xFFFFFFFF
+  GDK_GL_DONT_CARE                  = 0xFFFFFFFF
 } GdkGLDontCare;
 
 /*
@@ -161,7 +161,7 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_NONE                    = 0x8000
+  GDK_GL_NONE                       = 0x8000
 } GdkGLNone;
 
 /*
@@ -170,9 +170,9 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_CONFIG_CAVEAT_NONE      = GDK_GL_NONE,
-  GDK_GL_SLOW_CONFIG             = 0x8001,
-  GDK_GL_NON_CONFORMANT_CONFIG   = 0x800D
+  GDK_GL_CONFIG_CAVEAT_NONE         = GDK_GL_NONE,
+  GDK_GL_SLOW_CONFIG                = 0x8001,
+  GDK_GL_NON_CONFORMANT_CONFIG      = 0x800D
 } GdkGLConfigCaveat;
 
 /*
@@ -181,12 +181,12 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_TRUE_COLOR              = 0x8002,
-  GDK_GL_DIRECT_COLOR            = 0x8003,
-  GDK_GL_PSEUDO_COLOR            = 0x8004,
-  GDK_GL_STATIC_COLOR            = 0x8005,
-  GDK_GL_GRAY_SCALE              = 0x8006,
-  GDK_GL_STATIC_GRAY             = 0x8007
+  GDK_GL_TRUE_COLOR                 = 0x8002,
+  GDK_GL_DIRECT_COLOR               = 0x8003,
+  GDK_GL_PSEUDO_COLOR               = 0x8004,
+  GDK_GL_STATIC_COLOR               = 0x8005,
+  GDK_GL_GRAY_SCALE                 = 0x8006,
+  GDK_GL_STATIC_GRAY                = 0x8007
 } GdkGLVisualType;
 
 /*
@@ -195,9 +195,9 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_TRANSPARENT_NONE        = GDK_GL_NONE,
-  GDK_GL_TRANSPARENT_RGB         = 0x8008,
-  GDK_GL_TRANSPARENT_INDEX       = 0x8009
+  GDK_GL_TRANSPARENT_NONE           = GDK_GL_NONE,
+  GDK_GL_TRANSPARENT_RGB            = 0x8008,
+  GDK_GL_TRANSPARENT_INDEX          = 0x8009
 } GdkGLTransparentType;
 
 /*
@@ -206,9 +206,9 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_WINDOW_BIT              = 0x00000001,
-  GDK_GL_PIXMAP_BIT              = 0x00000002,
-  GDK_GL_PBUFFER_BIT             = 0x00000004
+  GDK_GL_WINDOW_BIT                 = 0x00000001,
+  GDK_GL_PIXMAP_BIT                 = 0x00000002,
+  GDK_GL_PBUFFER_BIT                = 0x00000004
 } GdkGLDrawableTypeMask;
 
 /*
@@ -217,8 +217,8 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_RGBA_BIT                = 0x00000001,
-  GDK_GL_COLOR_INDEX_BIT         = 0x00000002
+  GDK_GL_RGBA_BIT                   = 0x00000001,
+  GDK_GL_COLOR_INDEX_BIT            = 0x00000002
 } GdkGLRenderTypeMask;
 
 /*
@@ -227,14 +227,14 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_FRONT_LEFT_BUFFER_BIT   = 0x00000001,
-  GDK_GL_FRONT_RIGHT_BUFFER_BIT  = 0x00000002,
-  GDK_GL_BACK_LEFT_BUFFER_BIT    = 0x00000004,
-  GDK_GL_BACK_RIGHT_BUFFER_BIT   = 0x00000008,
-  GDK_GL_AUX_BUFFERS_BIT         = 0x00000010,
-  GDK_GL_DEPTH_BUFFER_BIT        = 0x00000020,
-  GDK_GL_STENCIL_BUFFER_BIT      = 0x00000040,
-  GDK_GL_ACCUM_BUFFER_BIT        = 0x00000080
+  GDK_GL_FRONT_LEFT_BUFFER_BIT      = 0x00000001,
+  GDK_GL_FRONT_RIGHT_BUFFER_BIT     = 0x00000002,
+  GDK_GL_BACK_LEFT_BUFFER_BIT       = 0x00000004,
+  GDK_GL_BACK_RIGHT_BUFFER_BIT      = 0x00000008,
+  GDK_GL_AUX_BUFFERS_BIT            = 0x00000010,
+  GDK_GL_DEPTH_BUFFER_BIT           = 0x00000020,
+  GDK_GL_STENCIL_BUFFER_BIT         = 0x00000040,
+  GDK_GL_ACCUM_BUFFER_BIT           = 0x00000080
 } GdkGLBufferMask;
 
 /*
@@ -243,8 +243,8 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_RGBA_TYPE               = 0x8014,
-  GDK_GL_COLOR_INDEX_TYPE        = 0x8015
+  GDK_GL_RGBA_TYPE                  = 0x8014,
+  GDK_GL_COLOR_INDEX_TYPE           = 0x8015
 } GdkGLRenderType;
 
 /*
@@ -253,11 +253,11 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_PRESERVED_CONTENTS      = 0x801B,
-  GDK_GL_LARGEST_PBUFFER         = 0x801C,
-  GDK_GL_WIDTH                   = 0x801D,
-  GDK_GL_HEIGHT                  = 0x801E,
-  GDK_GL_EVENT_MASK              = 0x801F
+  GDK_GL_PRESERVED_CONTENTS         = 0x801B,
+  GDK_GL_LARGEST_PBUFFER            = 0x801C,
+  GDK_GL_WIDTH                      = 0x801D,
+  GDK_GL_HEIGHT                     = 0x801E,
+  GDK_GL_EVENT_MASK                 = 0x801F
 } GdkGLDrawableAttrib;
 
 /*
@@ -278,7 +278,7 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_PBUFFER_CLOBBER_MASK    = 0x08000000
+  GDK_GL_PBUFFER_CLOBBER_MASK       = 0x08000000
 } GdkGLEventMask;
 
 /*
@@ -287,8 +287,8 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_DAMAGED                 = 0x8020,
-  GDK_GL_SAVED                   = 0x8021
+  GDK_GL_DAMAGED                    = 0x8020,
+  GDK_GL_SAVED                      = 0x8021
 } GdkGLEventType;
 
 /*
@@ -297,8 +297,8 @@ typedef enum
  */
 typedef enum
 {
-  GDK_GL_WINDOW                  = 0x8022,
-  GDK_GL_PBUFFER                 = 0x8023
+  GDK_GL_WINDOW                     = 0x8022,
+  GDK_GL_PBUFFER                    = 0x8023
 } GdkGLDrawableType;
 
 #ifdef __cplusplus
