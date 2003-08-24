6a2a0f0cdd488a5fd3d1d78c5002c6c44c35caa3
Naofumi Yasufuku <naofumi@users.sourceforge.net>
2003-08-24 08:42:47 +0000

    gen-*.pl scripts read common-header.h for common header part.

--- gdk/glext/gen-gdkglwglext-h.pl
+++ gdk/glext/gen-gdkglwglext-h.pl
@@ -11,25 +11,11 @@
 @input_headers = ("wglext.h", "wglext-extra.h");
 
 #---------------
-print <<EOF;
-/* GdkGLExt - OpenGL Extension to GDK
- * Copyright (C) 2002-2003  Naofumi Yasufuku
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
- */
+open(IN, "common-header.h") || die "cannot open common-header.h";
+print while (<IN>);
+close(IN);
 
+print <<EOF;
 /*
  * This is a generated file.  Please modify `gen-gdkglwglext-h.pl'.
  */
