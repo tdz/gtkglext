bf1057ab3382bb9be149a1be2c232a6694e52266
Ralf Corsepius <rcorsepius@src.gnome.org>
2009-12-26 16:52:49 +0100

    Acknowledge HAVE_CONFIG_H.

--- gtk/gtkglversion.c
+++ gtk/gtkglversion.c
@@ -16,6 +16,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "gtkglversion.h"
 
 const guint gtkglext_major_version = GTKGLEXT_MAJOR_VERSION;
