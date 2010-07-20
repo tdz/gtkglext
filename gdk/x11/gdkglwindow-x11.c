ea7d910127a4fc22d6481980fed49a93ee72cd10
Mukund Sivaraman <muks@banu.com>
2010-07-20 21:37:22 +0530

    Remove redundant parent_class variable

--- gdk/x11/gdkglwindow-x11.c
+++ gdk/x11/gdkglwindow-x11.c
@@ -47,8 +47,6 @@ static void gdk_gl_window_impl_x11_class_init (GdkGLWindowImplX11Class *klass);
 static void gdk_gl_window_impl_x11_finalize   (GObject                 *object);
 static void gdk_gl_window_impl_x11_gl_drawable_interface_init (GdkGLDrawableClass *iface);
 
-static gpointer parent_class = NULL;
-
 G_DEFINE_TYPE_EXTENDED (GdkGLWindowImplX11,
                         gdk_gl_window_impl_x11,
                         GDK_TYPE_GL_WINDOW,
@@ -68,8 +66,6 @@ gdk_gl_window_impl_x11_class_init (GdkGLWindowImplX11Class *klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
   GDK_GL_NOTE_FUNC_PRIVATE ();
-
-  parent_class = g_type_class_peek_parent (klass);
 
   object_class->finalize = gdk_gl_window_impl_x11_finalize;
 }
@@ -120,7 +116,7 @@ gdk_gl_window_impl_x11_finalize (GObject *object)
 
   g_object_unref (G_OBJECT (impl->glconfig));
 
-  G_OBJECT_CLASS (parent_class)->finalize (object);
+  G_OBJECT_CLASS (gdk_gl_window_impl_x11_parent_class)->finalize (object);
 }
 
 static void
