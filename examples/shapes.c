fd85468cab36de90b3aba8c16b04a48567988848
Thomas Zimmermann <tdz@users.sourceforge.net>
2012-01-07 15:17:45 +0100

    Converted program 'shapes' to GTK+ 3.
    Important changes include:
    
    - changing the GL widget's 'expose-event' to 'draw'.

--- examples/shapes.c
+++ examples/shapes.c
@@ -311,9 +311,9 @@ configure_event (GtkWidget         *widget,
 }
 
 static gboolean
-expose_event (GtkWidget      *widget,
-	      GdkEventExpose *event,
-	      gpointer        data)
+draw (GtkWidget *widget,
+	    cairo_t   *cr,
+	    gpointer   data)
 {
   GdkGLContext *glcontext = gtk_widget_get_gl_context (widget);
   GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (widget);
@@ -963,8 +963,8 @@ main (int   argc,
                           G_CALLBACK (realize), NULL);
   g_signal_connect (G_OBJECT (drawing_area), "configure_event",
 		    G_CALLBACK (configure_event), NULL);
-  g_signal_connect (G_OBJECT (drawing_area), "expose_event",
-		    G_CALLBACK (expose_event), NULL);
+  g_signal_connect (G_OBJECT (drawing_area), "draw",
+		    G_CALLBACK (draw), NULL);
 
   g_signal_connect (G_OBJECT (drawing_area), "button_press_event",
 		    G_CALLBACK (button_press_event), NULL);
