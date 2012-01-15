fe9f955323cf4db4cc76cf4fc19cf388df07c89d
Thomas.Zimmermann <tdz@users.sourceforge.net>
2012-01-15 12:39:03 +0100

    Remove unused variable in implementation of GdkGLContext.

--- gdk/gdkglcontext.c
+++ gdk/gdkglcontext.c
@@ -54,8 +54,6 @@ gdk_gl_context_init (GdkGLContext *self)
 static void
 gdk_gl_context_class_init (GdkGLContextClass *klass)
 {
-  GObjectClass *object_class = G_OBJECT_CLASS (klass);
-
   GDK_GL_NOTE_FUNC_PRIVATE ();
 
   klass->copy = NULL;
