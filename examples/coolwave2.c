ab8cb4718378cb8dacbb4fe7aa61c5ea3c6d7de4
Thomas Zimmermann <tdz@users.sourceforge.net>
2012-01-23 19:12:51 +0100

    coolwave2: Use begin/end interface of GTK+ widget.
    http://bugzilla.gnome.org/show_bug.cgi?id=642249

--- examples/coolwave2.c
+++ examples/coolwave2.c
@@ -235,12 +235,10 @@ static void
 realize (GtkWidget *widget,
 	 gpointer   data)
 {
-  GdkGLContext *glcontext = gtk_widget_get_gl_context (widget);
-  GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (widget);
   void (APIENTRY *PolygonOffset)(GLfloat, GLfloat) = NULL;
 
   /*** OpenGL BEGIN ***/
-  if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext))
+  if (!gtk_widget_begin_gl (widget))
     return;
 
   /* glPolygonOffsetEXT */
@@ -273,7 +271,7 @@ realize (GtkWidget *widget,
 
   resetWireframe ();
 
-  gdk_gl_drawable_gl_end (gldrawable);
+  gtk_widget_end_gl (widget, FALSE);
   /*** OpenGL END ***/
 
   return;
@@ -287,12 +285,9 @@ realize (GtkWidget *widget,
  ***/
 static gboolean
 configure_event (GtkWidget         *widget,
-		 GdkEventConfigure *event,
-		 gpointer           data)
+                 GdkEventConfigure *event,
+                 gpointer           data)
 {
-  GdkGLContext *glcontext = gtk_widget_get_gl_context (widget);
-  GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (widget);
-
   GLfloat w;
   GLfloat h;
 
@@ -300,13 +295,13 @@ configure_event (GtkWidget         *widget,
   h = event->height;
 
   /*** OpenGL BEGIN ***/
-  if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext))
+  if (!gtk_widget_begin_gl (widget))
     return FALSE;
 
   aspect = (float)w/(float)h;
   glViewport (0, 0, w, h);
 
-  gdk_gl_drawable_gl_end (gldrawable);
+  gtk_widget_end_gl (widget, FALSE);
   /*** OpenGL END ***/
 
   return TRUE;
@@ -322,11 +317,8 @@ draw (GtkWidget *widget,
       cairo_t   *event,
       gpointer   data)
 {
-  GdkGLContext *glcontext = gtk_widget_get_gl_context(widget);
-  GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable(widget);
-
   /*** OpenGL BEGIN ***/
-  if (!gdk_gl_drawable_gl_begin(gldrawable, glcontext))
+  if (!gtk_widget_begin_gl(widget))
     return FALSE;
 
   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -344,15 +336,7 @@ draw (GtkWidget *widget,
 
   drawWireframe ();
 
-  /* Swap buffers */
-  if (gdk_gl_drawable_is_double_buffered (gldrawable))
-    gdk_gl_drawable_swap_buffers (gldrawable);
-  else
-    glFlush ();
-
-  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  gdk_gl_drawable_gl_end (gldrawable);
+  gtk_widget_end_gl (widget, TRUE);
   /*** OpenGL END ***/
 
   return TRUE;
